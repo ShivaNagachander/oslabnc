63e93fb5fd8f7b2955407b360e44ada7587ec823
SHIVA NC <123239393+ShivaNagachander@users.noreply.github.com>
2024-09-25 18:56:02 +0530

    Update 8.opendir-closedir.c

--- syscalls/8.opendir-closedir.c
+++ syscalls/8.opendir-closedir.c
@@ -1,32 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <dirent.h>
-int main() 
-{
-    DIR *d;
-    struct dirent *de;
-    d = opendir(".");
-    if (d == NULL) 
-    {
-        perror("Unable to open directory");
-        return 1;
+int main(int argc, char *argv[]) {
+    DIR *dp;
+    struct dirent *dirp;
+
+    if (argc != 2) {
+        printf("A single argument (the directory name) is required.\n");
+        exit(1);
     }
-    while ((de = readdir(d)) != NULL) 
-    {
-        printf("%s\n", de->d_name);
+    if ((dp = opendir(argv[1])) == NULL) {
+        printf("Cannot open directory: %s\n", argv[1]);
+        exit(1);
     }
-    closedir(d);
+    while ((dirp = readdir(dp)) != NULL) {
+        printf("Name: %s | Inode: %ld\n", dirp->d_name, dirp->d_ino);
+    }
+    closedir(dp);
     return 0;
 }
 
-
-
-/tmp/q7orDbJtKq.o
-.
-..
-.bash_logout
-.bashrc
-.profile
-
-
-=== Code Execution Successful ===
+O/P:
+A single argument (the directory name) is required.
