fd9e0a44a97aef6c4b913d1ed72a5b4d03d052e6
SHIVA NC <123239393+ShivaNagachander@users.noreply.github.com>
2024-09-25 21:42:13 +0530

    Create sjf.c

---
+++ sjf.c
@@ -0,0 +1,99 @@
+
+#include<stdio.h>
+
+int main()
+
+{
+
+int p[20], bt[20], wt[20], tat[20], i, k, n, temp;
+
+float wtavg, tatavg;
+
+printf("\nEnter the number of processes -- ");
+
+scanf("%d", &n);
+
+for(i=0;i<n;i++)
+
+{
+
+p[i]=i;
+
+printf("Enter Burst Time for Process %d -- ", i);
+
+scanf("%d", &bt[i]);
+
+}
+
+
+
+for(i=0;i<n;i++)
+
+for(k=i+1;k<n;k++)
+
+if(bt[i]>bt[k])
+
+{
+
+temp=bt[i];
+
+bt[i]=bt[k];
+
+bt[k]=temp;
+
+temp=p[i];
+
+p[i]=p[k];
+
+p[k]=temp;
+
+}
+
+
+
+wt[0] = wtavg = 0;
+
+tat[0] = tatavg = bt[0];
+
+for(i=1;i<n;i++)
+
+{
+
+wt[i] = wt[i-1] +bt[i-1];
+
+tat[i] = tat[i-1] +bt[i];
+
+wtavg = wtavg + wt[i];
+
+tatavg = tatavg + tat[i];
+
+}
+
+printf("\n\t PROCESS \tBURST TIME \t WAITING TIME\t TURNAROUND TIME\n");
+
+
+for(i=0;i<n;i++)
+
+printf("\n\t P%d \t\t %d \t\t %d \t\t %d", p[i], bt[i], wt[i], tat[i]);
+
+printf("\nAverage Waiting Time -- %f", wtavg/n);
+
+printf("\nAverage Turnaround Time -- %f", tatavg/n);
+
+}
+
+
+Enter the number of processes -- 4
+Enter Burst Time for Process 0 -- 4
+Enter Burst Time for Process 1 -- 8
+Enter Burst Time for Process 2 -- 3
+Enter Burst Time for Process 3 -- 7
+
+         PROCESS        BURST TIME       WAITING TIME    TURNAROUND TIME
+
+         P2              3               0               3
+         P0              4               3               7
+         P3              7               7               14
+         P1              8               14              22
+Average Waiting Time -- 6.000000
+Average Turnaround Time -- 11.500000
