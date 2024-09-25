b286d2bdc8f0be48a7930f32b7e2eb4e6c8eddc2
SHIVA NC <123239393+ShivaNagachander@users.noreply.github.com>
2024-09-25 19:07:50 +0530

    Update 4.exit.c

--- syscalls/4.exit.c
+++ syscalls/4.exit.c
@@ -1,20 +1,16 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h> 
-int main() 
-{
-    int p = fork();
-    if (p == 0) 
-    {
-        printf("\nChild created\n");
-        exit(0); 
-    }
-    if (p < 0) 
-    {
-        printf("Can't create child\n");
+#include <stdlib.h>
+
+int main() {
+    int p = fork(); 
+    if (p == 0) {
+        printf("\nChild process created\n");
+        exit(0);
+    } else if (p < 0) {
+        printf("Failed to create child process\n");
         exit(-1); 
     }
-    printf("Process ended\n");
-    return 0; 
+    return 0;
 }
