785d771a36e929956eda764655db4e73e46159ae
SHIVA NC <123239393+ShivaNagachander@users.noreply.github.com>
2024-09-26 10:19:56 +0530

    Update 3.getpid.c

--- syscalls/3.getpid.c
+++ syscalls/3.getpid.c
@@ -3,6 +3,11 @@
 #include<sys/types.h>
 int main()
 {
-printf("\n parent process id %d",c());
+printf("\n parent process id %d",getppid());
 printf("\n child process id %d\n",getpid());
 }
+
+
+ parent process id 17861
+ child process id 17868
+
